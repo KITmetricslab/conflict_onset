dac240cab9958ad5ef7a973d61b2107994bd0905
Tobias Bodentien <bodentien.tobias@gmail.com>
2024-11-28 16:43:13 +0100

    outbreak dataset

--- data_analysis.R
+++ data_analysis.R
@@ -28,8 +28,6 @@ data_from_1990 <- data_from_1990 %>% select(country_id, month_id, ged_sb)
 # it has the columns "outbreak_level", "peace_months_prior", "conflict_months_after", "month_id", "country_id"
 
 ### do for every row (that is for every country and every month) so at time t(country)
-## column "outbreak_level":
-# if fatalities(t-1) = 0  and fatalities (t) > 0 then outbreak_level(t) = fat(t) - fat(t-1) = fat(t)
 ## column "peace_months_prior":
 # if outbreak_level(t) > 0 then 
 # peace_months_prior(t) = |{z| z in [z_start, t), fat(z) = 0, z_start = min{x in [z_start, t)| f(x)=0 and f(x - 1) > 0}}| 
@@ -45,9 +43,8 @@ outbreak_data <- data.frame(outbreak_level = integer(),
                             country_id = integer())
 
 
-### Achtung vorläufig und nicht fertig!
 
-# iterate over all rows of the data_2018_to_2023 data frame
+## iterate over all rows of the data_2018_to_2023 data frame
 for (i in 1:nrow(data_2018_to_2023)) {
   # get the country_id and month_id of the current row
   country_id <- data_2018_to_2023$country_id[i]
@@ -56,29 +53,118 @@ for (i in 1:nrow(data_2018_to_2023)) {
   # get the fatality count of the current row
   fatality_count <- data_2018_to_2023$outcome[i]
   
-  # get the fatality count of the previous month
-  if month_id = 457:
-    #fatality_count_previous_month <- # data_from_1990$ged_sb[which(data_from_1990$country_id == country usw.
-  else:
-    fatality_count_previous_month <- data_2018_to_2023$ged_sb[i - 1]
+  # initialize variables
+  outbreak_level <- 0
+  fatality_count_previous_month <- NA
   
+  ## column "outbreak_level":
+  # get the fatality count of the previous month
+  if (month_id == 457){
+    fatality_count_previous_month <- data_from_1990$ged_sb[which(data_from_1990$month_id == month_id - 1 & 
+                                                                   data_from_1990$country_id == country_id)]
+  } else{
+    fatality_count_previous_month <- data_2018_to_2023$outcome[which(data_2018_to_2023$month_id == month_id - 1 & 
+                                                                      data_2018_to_2023$country_id == country_id)]
+  }
   # outbreak level
-  outbreak_level <- fatality_count - fatality_count_previous_month
+  if(fatality_count_previous_month == 0 & fatality_count > 0){
+    outbreak_level <- fatality_count - fatality_count_previous_month
+  }
+  
+
 
   
-  
-  
-  # peace months prior
-  
-  
-  
-  # conflict months after
-  
-  
-  if outbreak_level > 0:
-    # add the data to the outbreak_data data frame
+  if(outbreak_level > 0){
+    
+    ## column "peace_months_prior":
+    fatalities_month_prior <- fatality_count_previous_month
+    prior_outbreak_month_id <- month_id
+    peace_months_prior_count <- 0
+    
+    
+    while(fatalities_month_prior == 0){
+        peace_months_prior_count <- peace_months_prior_count + 1
+        
+        
+        prior_outbreak_month_id <- prior_outbreak_month_id - 1
+        
+        if (prior_outbreak_month_id <= 457){
+          fatalities_month_prior <- data_from_1990$ged_sb[which(data_from_1990$month_id == prior_outbreak_month_id & 
+                                                                         data_from_1990$country_id == country_id)]
+        } else{
+          fatalities_month_prior <- data_2018_to_2023$outcome[which(data_2018_to_2023$month_id == prior_outbreak_month_id & 
+                                                                             data_2018_to_2023$country_id == country_id)]
+        }
+        
+        if(length(fatalities_month_prior) == 0){
+          break
+        }
+    }
+    
+    ## column "conflict_months_after
+    past_outbreak_month_id <- month_id + 1
+    
+    if(month_id == 528){
+      fatalities_month_past = 0
+    } else{
+      fatalities_month_past <- data_2018_to_2023$outcome[which(data_2018_to_2023$month_id == past_outbreak_month_id & 
+                                                                 data_2018_to_2023$country_id == country_id)]
+    }
+    
+    
+    conflict_months_past_count <- 0
+    
+    while(fatalities_month_past > 0){
+      conflict_months_past_count <- conflict_months_past_count + 1
+      
+      past_outbreak_month_id <- past_outbreak_month_id + 1
+      
+      fatalities_month_past <- data_2018_to_2023$outcome[which(data_2018_to_2023$month_id == past_outbreak_month_id & 
+                                                                    data_2018_to_2023$country_id == country_id)]
+      
+      if(length(fatalities_month_past) == 0){
+        break
+      }
+      
+    }
+    
+    ## add the data to the outbreak_data data frame
     outbreak_data <- rbind(outbreak_data, data.frame(outbreak_level = outbreak_level, 
-                                                     peace_months_prior = peace_months_prior, 
-                                                     conflict_months_after = conflict_months_after, 
-                                                     month_id = month_id)
+                                                     peace_months_prior = peace_months_prior_count, 
+                                                     conflict_months_after = conflict_months_past_count, 
+                                                     month_id = month_id,
+                                                     country_id = country_id))
+    
+  }  
 }
+
+
+# number of outbreaks per country
+outbreak_counts <- outbreak_data %>%
+  group_by(country_id) %>%
+  summarise(number_of_outbreaks = n())
+
+# subset for different countries
+country = 117
+subset_country <- data_2018_to_2023 %>%
+  filter(country_id == country)
+
+
+# histogram for the conflict outbreaks
+ggplot(outbreak_data, aes(x = outbreak_level)) +
+  geom_histogram(binwidth = log10(1.1), fill = "blue", color = "black", alpha = 0.1) +
+  scale_x_log10() +
+  labs(
+    title = "Histogram Outbreak-Level",
+    x = "Outbreak-Level",
+    y = "H(x)"
+  ) +
+  theme_minimal()
+
+## plot #outbreaks per country vs. avrg. outbreak magnitude
+
+## plot time (in months) vs. #outbreaks
+
+
+
+
